ac103cb5ae8cd4dcb4f6b5cb9007acc02418e12d
Jeasika López <jesika_18_@hotmail.com>
2022-10-28 08:57:37 -0500

    Open file added

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 let extension = [".com.mx", ".lat", ".org.mx", ".net", ".cc", ".tv"];
 let adjetives = ["happy", "very", "best", "cool", "epic"];
 let extensionForAdjetives = [".com", ".mx"];
+const {readFileSync, promises: fsPromises} = require('fs');
 
+//Read file
+async function asyncReadFile(filename) {
+  try {
+    const contents = await fsPromises.readFile(filename, 'utf-8');
+    const arr = contents.split(/\r?\n/);
+    console.log(arr); // 👉️ ['One', 'Two', 'Three', 'Four']
+    return arr;
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-function generateRandom(min = 0, max = 4) {
-  // find diff
+//Function to generate a random number
+function generateRandom(min = 0, max = 5) {
   let difference = max - min;
   // generate random number 
   let rand = Math.random();
-  // multiply with difference 
   rand = Math.floor( rand * difference);
-  // add with min value 
-  rand = rand + min;
   return rand;
 }
 
+//Function to generate domain name options
 function generateDomain() {
+  asyncReadFile('D:\Jesik\Documents\GitHub\Semanai\List_Dom.csv');
   document.getElementById("listaDominios").innerHTML = "";
   const palabra = document.getElementById('searchName').value;
   alert(palabra);
+
   //For each extension
   for(let i=0; i<extension.length; i++){
     let nuevoNombre = document.createTextNode( palabra.replace(/\s/g, '') + extension[i]);
+    //Si existe nuevo nombre
     let lista = document.getElementById("listaDominios");
     let elemento = document.createElement("div");
     lista.appendChild(elemento);
@@ -33,6 +46,8 @@ function generateDomain() {
     seleccionador.setAttribute("value", nuevoNombre.textContent);
     elemento.appendChild(seleccionador);
   }
+
+
   //For a random number by some adjetives
   for(let i=0; i<extensionForAdjetives.length; i++){
     let numeroRandom = generateRandom();
@@ -49,6 +64,7 @@ function generateDomain() {
     seleccionador.setAttribute("value", nuevoNombre.textContent);
     elemento.appendChild(seleccionador);
   }
+
   //Open AI
 
 }
