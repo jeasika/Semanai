02d10017a6998d3b5105594e5a23021170761f3c
Jeasika López <jesika_18_@hotmail.com>
2022-10-28 11:26:18 -0500

    update js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
+let data = ["prolace.net", "cardiotrace.lat", "friomaya.net", "idiomachino.net", "upc.lat", "protege.lat", "maceta.lat", "colegioolinca.net", "renta.lat", "copyfunnels.lat", "neuroalimentacion.net", "dondever.net", "evolitivo.cc", "hayashi.lat", "dobot.lat", "soulzenter.lat", "endemika.lat", "dolartrac.cc", "intelissarh.lat", "e-watch.tv", "gsb.lat", "matribu.lat", "consumibles.lat", "estrasol.lat", "laticinio.lat", "flores.lat", "terramarusa.cc", "cotailor.net", "enrecluta.net", "luliloopsy.lat", "tutarjeta.lat", "torrechapultepec.net", "smartbike.tv", "henequeneros.cc", "nog.lat", "mercadoabierto.net", "intercamseguros.lat", "pricetravel.lat", "skipware.net", "tutiendaenred.net"];
 let extension = [".com.mx", ".lat", ".org.mx", ".net", ".cc", ".tv"];
 let adjetives = ["happy", "very", "best", "cool", "epic"];
 let extensionForAdjetives = [".com", ".mx"];
-const {readFileSync, promises: fsPromises} = require('fs');
 
-//Read file
-async function asyncReadFile(filename) {
-  try {
-    const contents = await fsPromises.readFile(filename, 'utf-8');
-    const arr = contents.split(/\r?\n/);
-    console.log(arr); // 👉️ ['One', 'Two', 'Three', 'Four']
-    return arr;
-  } catch (err) {
-    console.log(err);
+//Function to validate words
+function available(word){
+  for(let i=0; i<data.length; i++){
+    console.log(data[i]);
+    if (word == data[i]){
+      console.log("DUPLICADO");
+      return false;
+    }
+    else {
+      return true;
+    }
   }
 }
 
@@ -26,25 +28,32 @@ function generateRandom(min = 0, max = 5) {
 
 //Function to generate domain name options
 function generateDomain() {
-  asyncReadFile('D:\Jesik\Documents\GitHub\Semanai\List_Dom.csv');
   document.getElementById("listaDominios").innerHTML = "";
   const palabra = document.getElementById('searchName').value;
-  alert(palabra);
+  //alert(palabra);
+  //console.log(available(palabra));
 
   //For each extension
   for(let i=0; i<extension.length; i++){
     let nuevoNombre = document.createTextNode( palabra.replace(/\s/g, '') + extension[i]);
     //Si existe nuevo nombre
-    let lista = document.getElementById("listaDominios");
-    let elemento = document.createElement("div");
-    lista.appendChild(elemento);
-    elemento.appendChild(nuevoNombre);
-    let seleccionador = document.createElement("input");
-    seleccionador.className = "form-check-input";
-    seleccionador.setAttribute("type", "radio");
-    seleccionador.setAttribute("name", "opcion");
-    seleccionador.setAttribute("value", nuevoNombre.textContent);
-    elemento.appendChild(seleccionador);
+    console.log(nuevoNombre);
+    if (available(nuevoNombre) == false){
+      console.log(nuevoNombre);
+    }
+    else{
+      let lista = document.getElementById("listaDominios");
+      let elemento = document.createElement("div");
+      lista.appendChild(elemento);
+      elemento.appendChild(nuevoNombre);
+      let seleccionador = document.createElement("input");
+      seleccionador.className = "form-check-input";
+      seleccionador.setAttribute("type", "radio");
+      seleccionador.setAttribute("name", "opcion");
+      seleccionador.setAttribute("value", nuevoNombre.textContent);
+      elemento.appendChild(seleccionador);
+    }
+
   }
 
 
@@ -65,6 +74,9 @@ function generateDomain() {
     elemento.appendChild(seleccionador);
   }
 
+  //for(let i=0; i<data.length; i++){
+    //console.log(data[i]);
+  //}
   //Open AI
 
 }
